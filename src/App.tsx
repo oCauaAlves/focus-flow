e2651185b7c815fdd47af8896e90ddec6d3ccc45
Caua Alves <caua.rodriguesalves2004@gmail.com>
2026-06-12 15:03:11 -0300

    feat: trabalhando no css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import './style/theme.css';
-import './style/global.css';
-import { Heading } from './components/Heading';
 
+import './styles/theme.css';
+import './styles/global.css';
 
 export function App() {
-    return (
-     <Heading/>
-    );
-}
+  return <>
+      <div className ='container'>
+        <div className="content">
+          <section> LOGO </section>  
+        </div> 
+      </div>
 
+      <div className ='container'>
+        <div className="content">
+          <section> MENU </section>  
+        </div> 
+      </div>
+
+      <div className ='container'>
+        <div className="content">
+          <section> FORM </section>  
+        </div> 
+      </div>
+
+      <div className ='container'>
+        <div className="content">
+          <section> FOOTER </section>  
+        </div> 
+      </div>
+  </>;
+}
